93b5f1a64eb8fa58fffeb07923d269ed6013f5ed
cddorz <2878153350@qq.com>
2021-08-05 10:24:19 +0800

    feat 投票法找到小镇法官

---
+++ src/图系列/lc997.java
@@ -0,0 +1,24 @@
+package src.图系列;
+
+/**
+ * @author hly
+ * @Description: 找到小镇法官
+ * @create 2021-08-05 10:13
+ */
+public class lc997 {
+    public int findJudge(int n, int[][] trust) {
+        int[] votes = new int[];
+        for(int[] vote : trust){
+            votes[vote[1]]++;
+            votes[vote[0]]--;
+        }
+        int judge = -1;
+        for (int i = 0; i < n; i++) {
+            if(votes[i] == n - 1){
+                judge = i;
+                break;
+            }
+        }
+        return judge;
+    }
+}
